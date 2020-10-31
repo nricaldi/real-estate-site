dee5ba5d738a68624047d42dd479e862b22bc067
Nicolas Ricaldi <nicolasricaldi@Nicolass-MBP.fios-router.home>
2020-10-31 14:13:01 -0400

    created realtor and listing models

--- listings/urls.py
+++ listings/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='listings'),
-    path('/listings/<int:listing_id>', views.listing, name='listing'),
+    path('listings/<int:listing_id>', views.listing, name='listing'),
     path('search', views.search, name='search'),
 ] 
